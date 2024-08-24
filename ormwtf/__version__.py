3321b63b59ef1acc976de36afa35e8b21e9c5185
Lev Litvinov <misery7100@gmail.com>
2024-08-24 03:56:10 +0300

    Add empty test files

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0.post22.dev0+6d553f0"
-__version_tuple__ = (0, 0, 0, "post22", "dev0", "6d553f0")
+__version__ = "0.0.0.post23.dev0+1131036"
+__version_tuple__ = (0, 0, 0, "post23", "dev0", 1131036)
