9e4cde2aa7be4fcf3a6e8513a42014a4e3b2015f
Lev Litvinov <misery7100@gmail.com>
2024-08-24 12:27:06 +0300

    Fix version format

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0.post27.dev0+e9c216c"
-__version_tuple__ = (0, 0, 0, "post27", "dev0", "e9c216c")
+__version__ = "0.0.0.dev28+g37803d9"
+__version_tuple__ = (0, 0, 0, "dev28", "g37803d9")
