947978501afe0b99d46c02913d70ff45699100f9
Lev Litvinov <misery7100@gmail.com>
2024-08-24 23:39:14 +0300

    Update testing hook

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.1-pre.3+057f4b6"
-__version_tuple__ = (0, 1, 1, "pre", 3, "057f4b6")
+__version__ = "0.1.1-pre.4+3458f3e"
+__version_tuple__ = (0, 1, 1, "pre", 4, "3458f3e")
