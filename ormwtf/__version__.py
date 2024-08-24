8f41c508b920ab6ca57c5bd1db0f452cde1f01dc
Lev Litvinov <misery7100@gmail.com>
2024-08-24 03:57:27 +0300

    Fix tox config

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0.post23.dev0+1131036"
-__version_tuple__ = (0, 0, 0, "post23", "dev0", 1131036)
+__version__ = "0.0.0.post24.dev0+3321b63"
+__version_tuple__ = (0, 0, 0, "post24", "dev0", "3321b63")
