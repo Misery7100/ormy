3dfd33d80a7b4a3589953530fce0164c4722aad2
Lev Litvinov <misery7100@gmail.com>
2024-08-25 00:51:11 +0300

    Add py.typed

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.1-pre.7+18d640a"
-__version_tuple__ = (0, 1, 1, "pre", 7, "18d640a")
+__version__ = "0.1.1-pre.8+382948b"
+__version_tuple__ = (0, 1, 1, "pre", 8, "382948b")
