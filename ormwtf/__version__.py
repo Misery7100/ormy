18d640a5f1c66e49c1e451db050b4737121a270d
Lev Litvinov <misery7100@gmail.com>
2024-08-24 23:42:23 +0300

    Fix hook

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.1-pre.5+9479785"
-__version_tuple__ = (0, 1, 1, "pre", 5, 9479785)
+__version__ = "0.1.1-pre.6+e287271"
+__version_tuple__ = (0, 1, 1, "pre", 6, "e287271")
