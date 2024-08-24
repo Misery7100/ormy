37803d9eb44ce1782cdaa12f157c5a0423737924
Lev Litvinov <misery7100@gmail.com>
2024-08-24 04:02:39 +0300

    Ver upd (?)

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.0.0.post26.dev0+ddc6678"
-__version_tuple__ = (0, 0, 0, "post26", "dev0", "ddc6678")
+__version__ = "0.0.0.post27.dev0+e9c216c"
+__version_tuple__ = (0, 0, 0, "post27", "dev0", "e9c216c")
