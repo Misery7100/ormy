8855102a671ae61203045d6f1550f476d0336444
Lev Litvinov <misery7100@gmail.com>
2024-08-25 19:36:45 +0300

    Fix redis dep

--- ormwtf/__version__.py
+++ ormwtf/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.1-pre.16+3b39d23"
-__version_tuple__ = (0, 1, 1, "pre", 16, "3b39d23")
+__version__ = "0.1.1-pre.17+171a447"
+__version_tuple__ = (0, 1, 1, "pre", 17, "171a447")
