7defb0007ef61790f4a16772e141882dfc8922a7
Lev Litvinov <misery7100@gmail.com>
2024-12-29 01:54:55 +0300

    Bump mongo deps

--- ormy/__version__.py
+++ ormy/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.3-pre.98+4bf0df5"
-__version_tuple__ = (0, 1, 3, "pre", 98, "4bf0df5")
+__version__ = "0.1.3-pre.99+7079d8b"
+__version_tuple__ = (0, 1, 3, "pre", 99, "7079d8b")
