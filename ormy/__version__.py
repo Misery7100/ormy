c8e762cd3726cb7d94a24a25ece5a213b7c77d32
Lev Litvinov <misery7100@gmail.com>
2025-01-13 07:00:35 +0300

    cache addon

--- ormy/__version__.py
+++ ormy/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.3-pre.160+9ff3def"
-__version_tuple__ = (0, 1, 3, "pre", 160, "9ff3def")
+__version__ = "0.1.3-pre.161+dd67e99"
+__version_tuple__ = (0, 1, 3, "pre", 161, "dd67e99")
---
+++ ormy/addon/aiocache.py
@@ -0,0 +1,318 @@
+import asyncio
+import functools
+import json
+from typing import Any, Dict, List, Optional
+
+import anyio
+from aiocache import Cache as AioCache  # type: ignore[import-untyped]
+from aiocache import cached as aiocache_cached  # type: ignore[import-untyped]
+from aiocache.decorators import _get_cache, logger  # type: ignore[import-untyped]
+from aiocache.factory import caches  # type: ignore[import-untyped]
+
+from ormy.base.error import InternalError
+
+# ----------------------- #
+
+
+class _cached(aiocache_cached):
+    """
+    Subclass of `.aiocache_cached` decorator that supports synchronous functions.
+    """
+
+    def __call__(self, f):
+        if self.alias:
+            self.cache = caches.get(self.alias)
+            for arg in ("serializer", "namespace", "plugins"):
+                if getattr(self, f"_{arg}", None) is not None:
+                    logger.warning(f"Using cache alias; ignoring '{arg}' argument.")
+        else:
+            self.cache = _get_cache(
+                cache=self._cache,
+                serializer=self._serializer,
+                namespace=self._namespace,
+                plugins=self._plugins,
+                **self._kwargs,
+            )
+
+        if asyncio.iscoroutinefunction(f):
+
+            @functools.wraps(f)
+            async def async_wrapper(*args, **kwargs):
+                return await self.decorator(f, *args, **kwargs)
+
+            async_wrapper.cache = self.cache  # type: ignore
+            return async_wrapper
+
+        else:
+
+            @functools.wraps(f)
+            def wrapper(*args, **kwargs):
+                return anyio.run(self.decorator, f, *args, **kwargs)
+
+            wrapper.cache = self.cache  # type: ignore
+            return wrapper
+
+    # ....................... #
+
+    async def decorator(
+        self,
+        f,
+        *args,
+        cache_read=True,
+        cache_write=True,
+        aiocache_wait_for_write=True,
+        **kwargs,
+    ):
+        key = self.get_cache_key(f, args, kwargs)
+
+        if cache_read:
+            value = await self.get_from_cache(key)
+            if value is not None:
+                return value
+
+        if asyncio.iscoroutinefunction(f):
+            result = await f(*args, **kwargs)
+
+        else:
+            result = f(*args, **kwargs)
+
+        if self.skip_cache_func(result):
+            return result
+
+        if cache_write:
+            if aiocache_wait_for_write:
+                await self.set_in_cache(key, result)
+            else:
+                # TODO: Use aiojobs to avoid warnings.
+                asyncio.create_task(self.set_in_cache(key, result))
+
+        return result
+
+
+# ....................... #
+
+
+def _key_factory(
+    plain: Optional[str] = None,
+    from_keys: Optional[List[str]] = None,
+):
+    """
+    Create a cache key for a function.
+
+    Args:
+        plain (Optional[str]): A plain string to use as the cache key.
+        from_keys (Optional[List[str]]): A list of keys to use as the cache key.
+
+    Returns:
+        key_builder (Callable): A function that creates a cache key for a function.
+    """
+
+    _as_is_tuple = (str, int, float, bool, list, dict, tuple, set)
+
+    def _safe_dump(v: Any):
+        """
+        Safely dump a value to a string.
+        """
+
+        if not isinstance(v, _as_is_tuple):
+            v = json.dumps(v, sort_keys=True, default=str)
+
+        return v
+
+    def key_builder(f, *args, **kwargs):
+        arg_names: List[str] = f.__code__.co_varnames[: f.__code__.co_argcount]
+        defaults = f.__defaults__ or ()
+        pos_defaults = dict(zip(arg_names[-len(defaults) :], defaults))
+        key_dict: Dict[str, Any] = {}
+        self_or_cls = args[0]
+
+        if from_keys is None:
+            if plain is None:
+                raise InternalError("Either `plain` or `from_keys` must be provided")
+
+            return plain
+
+        else:
+            for u in from_keys:
+                if u.startswith("self."):
+                    attr = u.split(".")[1]
+                    key_dict[attr] = _safe_dump(getattr(self_or_cls, attr))
+
+                elif u in kwargs:
+                    key_dict[u] = _safe_dump(kwargs[u])
+
+                elif len(args) > arg_names.index(u):
+                    key_dict[u] = _safe_dump(args[arg_names.index(u)])
+
+                else:
+                    key_dict[u] = pos_defaults[u]
+
+        return ";".join([f"{k}={v}" for k, v in key_dict.items()])
+
+    return key_builder
+
+
+# ....................... #
+
+_ID_ALIASES = ["_id", "id_"]
+
+
+def _extract_namespace(target_entity, target_id, func, self_or_cls, *args, **kwargs):
+    if target_id is None:
+        arg_names: List[str] = func.__code__.co_varnames[: func.__code__.co_argcount][
+            1:
+        ]
+        id_arg_name = next((n for n in arg_names if n in _ID_ALIASES), None)
+
+        if id_arg_name is None:
+            if not hasattr(self_or_cls, "id"):
+                raise InternalError(f"`{func.__name__}` does not have an id argument")
+
+            _id = getattr(self_or_cls, "id")
+
+        else:
+            if id_arg_name in kwargs.keys():
+                _id = kwargs[id_arg_name]
+
+            else:
+                _id = args[arg_names.index(id_arg_name)]
+
+    else:
+        _id = target_id
+
+    if target_entity is None:
+        if hasattr(self_or_cls, "_get_entity") and callable(self_or_cls._get_entity):
+            namespace = self_or_cls._get_entity()
+
+        else:
+            raise InternalError(f"{self_or_cls} does not have a '_get_entity' method")
+
+    else:
+        namespace = target_entity
+
+    return namespace, _id
+
+
+# ....................... #
+
+
+def cache(
+    plain: Optional[str] = None,
+    from_keys: Optional[List[str]] = None,
+    **cache_kwargs,
+):
+    """
+    Decorator to cache a function result.
+
+    Args:
+        plain (Optional[str]): The plain string to use as the cache key.
+        from_keys (Optional[List[str]]): The keys to use as the cache key.
+        **cache_kwargs: The cache kwargs.
+
+    Returns:
+        decorator (Callable): The decorator to cache a function.
+    """
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(self_or_cls, *args, **kwargs):
+            namespace, _id = _extract_namespace(
+                None,
+                None,
+                func,
+                self_or_cls,
+                *args,
+                **kwargs,
+            )
+
+            cache_kwargs["key_builder"] = _key_factory(plain, from_keys)
+            cache_kwargs["namespace"] = f"{namespace}:{_id}"
+
+            return _cached(**cache_kwargs)(func)(self_or_cls, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+# ....................... #
+
+
+def cache_clear(
+    target_entity: Optional[str] = None,
+    target_id: Optional[str] = None,
+    keys: Optional[List[str]] = None,
+    **cache_kwargs,
+):
+    """
+    Decorator to clear the cache
+
+    Args:
+        target_entity (Optional[str]): The entity to clear the cache for.
+        target_id (Optional[str]): The id to clear the cache for.
+        keys (Optional[List[str]]): The keys to clear the cache for.
+
+    Returns:
+        decorator (Callable): The decorator to clear the cache.
+    """
+
+    def decorator(func):
+        if asyncio.iscoroutinefunction(func):
+
+            @functools.wraps(func)
+            async def async_wrapper(self_or_cls, *args, **kwargs):
+                namespace, _id = _extract_namespace(
+                    target_entity,
+                    target_id,
+                    func,
+                    self_or_cls,
+                    *args,
+                    **kwargs,
+                )
+
+                res = await func(self_or_cls, *args, **kwargs)
+
+                cache_kwargs["namespace"] = f"{namespace}:{_id}"
+                cache = AioCache(**cache_kwargs)
+
+                if keys:
+                    for k in keys:
+                        await cache.delete(k)  # type: ignore
+
+                else:
+                    await cache.clear()  # type: ignore
+
+                return res
+
+            return async_wrapper
+
+        else:
+
+            @functools.wraps(func)
+            def sync_wrapper(self_or_cls, *args, **kwargs):
+                namespace, _id = _extract_namespace(
+                    target_entity,
+                    target_id,
+                    func,
+                    self_or_cls,
+                    *args,
+                    **kwargs,
+                )
+
+                res = func(self_or_cls, *args, **kwargs)
+
+                cache_kwargs["namespace"] = f"{namespace}:{_id}"
+                cache = AioCache(**cache_kwargs)
+
+                if keys:
+                    for k in keys:
+                        anyio.run(cache.delete, k)  # type: ignore
+
+                else:
+                    anyio.run(cache.clear)  # type: ignore
+
+                return res
+
+            return sync_wrapper
+
+    return decorator
