b26d0f4fb59a8e904a2cd214ae29f4c40aa17472
Lev Litvinov <misery7100@gmail.com>
2025-02-02 13:14:02 +0300

    Add boilerplate

--- ormy/__version__.py
+++ ormy/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.3-pre.191+b4a4b60"
-__version_tuple__ = (0, 1, 3, "pre", 191, "b4a4b60")
+__version__ = "0.1.3-pre.192+38518ec"
+__version_tuple__ = (0, 1, 3, "pre", 192, "38518ec")
