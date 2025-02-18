94e97a9f205c2ea927c01a424db0aebe3410a50d
Lev Litvinov <misery7100@gmail.com>
2025-02-18 17:01:19 +0300

    Fix extra name

--- ormy/__version__.py
+++ ormy/__version__.py
@@ -1,3 +1,3 @@
 # These version placeholders will be replaced later during substitution.
-__version__ = "0.1.4"
-__version_tuple__ = (0, 1, 4)
+__version__ = "0.1.5-pre.1+e4a35cd"
+__version_tuple__ = (0, 1, 5, "pre", 1, "e4a35cd")
