aac5981e976bde7bb416df8728ca9f1bdfde06f8
Lev Litvinov <misery7100@gmail.com>
2025-04-10 15:12:55 +0300

    Fix fix fix

--- ormy/document/arango/wrapper.py
+++ ormy/document/arango/wrapper.py
@@ -697,9 +697,9 @@ class ArangoBaseEdge(ArangoBase):
 class ArangoEdgeDefinition(BaseModel):
     """ArangoDB edge definition"""
 
-    edge_collection: ClassVar[type[ArangoBaseEdge]]
-    from_nodes: ClassVar[list[type[ArangoBase]]]
-    to_nodes: ClassVar[list[type[ArangoBase]]]
+    edge_collection: type[ArangoBaseEdge]
+    from_nodes: list[type[ArangoBase]]
+    to_nodes: list[type[ArangoBase]]
 
 
 # ....................... #
